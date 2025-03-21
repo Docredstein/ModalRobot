37d89a3b9570ac996e684e7171022f155990fd56
FRAMBOURT Mateïs <mateis.frambourt@polytechnique.edu>
2025-03-21 16:04:37 +0100

    first commit

---
+++ MotorControl/build/CMakeFiles/MotorControl.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MotorControl.dir/MotorControl.cpp.o"
+  "CMakeFiles/MotorControl.dir/MotorControl.cpp.o.d"
+  "libMotorControl.a"
+  "libMotorControl.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MotorControl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ PID/PID.cpp
@@ -0,0 +1,35 @@
+#include "PID.hpp"
+
+
+PID::PID(float kp, float ki, float kd,float maxAccumulator)
+{
+    m_Kp = kp;
+    m_Ki = ki;
+    m_Kd = kd;
+    m_accumulator =0;
+    m_lastMicros=micros();
+    m_maxAccumulator=maxAccumulator;
+    m_initialized = false;
+    m_lastError=0;
+}
+
+PID::~PID()
+{
+}
+
+float PID::update(float error) {
+
+    //float error = target - measure;
+    long time = micros();
+    if (m_Ki!=0) {
+    m_accumulator+=error*(time-m_lastMicros)*1e-6;
+    m_accumulator = (m_accumulator>m_maxAccumulator)?m_maxAccumulator:m_accumulator;
+    }
+    float out = m_Kp*error+m_Ki*m_accumulator+m_Kd*(error - m_lastError)/(time-m_lastMicros);
+    m_lastError=error;
+    m_lastMicros=time;
+    return out;
+
+    
+
+}
